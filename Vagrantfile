9bf0b566a2cd1d02615fbdf480a40b5168e94ed5
cian <cian@cixce.com>
2016-07-23 13:19:14 -0700

    Fix config

--- Vagrantfile
+++ Vagrantfile
@@ -5,17 +5,17 @@
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-	config.vm.box = "ubuntu/xenial64"
+    config.vm.box = "boxcutter/ubuntu1604-desktop"
   config.vm.provision "shell", path: "provision.sh"
 
   config.vm.synced_folder File.expand_path('~'), "/home/vagrant/home"
-  
+
   config.vm.provider "virtualbox" do |vb|
     vb.name = "pwn"
     vb.memory = "1024"
-	vb.gui = true
+    vb.gui = true
+    vb.customize ["modifyvm", :id, '--audio', 'dsound', '--audiocontroller', 'ac97']
   end
-  
+
   config.ssh.forward_agent = true
 end
-
--- provision.sh
+++ provision.sh
@@ -2,15 +2,25 @@
 
 
 
-echo deb http://deb.torproject.org/torproject.org jessie main >> etc/apt/sources.list.d/tor.list
-echo deb-src http://deb.torproject.org/torproject.org jessie main >> /etc/apt/sources.list.d/tor.ltst
+echo deb http://deb.torproject.org/torproject.org jessie main >> /etc/apt/sources.list.d/tor.list
+echo deb-src http://deb.torproject.org/torproject.org jessie main >> /etc/apt/sources.list.d/tor.list
+
+apt-get update
+apt-get upgrade
 
 
 gpg --keyserver keys.gnupg.net --recv 886DDD89
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
 
 apt-get update
+apt-get upgrade
+#apt-get install -y tor deb.torproject.org-keyring
+apt-get install -y flashplugin-installer
 
-apt-get install -y ubuntu-desktop
-apt-get install -y tor deb.torproject.org-keyring
-
+cd /hom/vagrant/
+mkdir opt
+cd opt
+wget --progress=dot https://www.torproject.org/dist/torbrowser/6.5a1/tor-browser-linux64-6.5a1_en-US.tar.xz
+tar -xvJf tor-browser-linux64-6.5a1_en-US.tar.xz
+cd ..
+chown vagrant.vagrant -R opt
